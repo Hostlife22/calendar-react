66777a77aad362f041e7f0f50c8e8171fafaef41
Hostlife22 <sima.serafim2021@mail.ru>
2022-02-06 18:14:31 +0300

    feat: added style

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,7 +15,14 @@ const Header = () => {
           className="button create-event-btn"
           onClick={() => setModal({ visable: true, defaultValue: null })}
         >
-          <i className="fas fa-plus create-event-btn__icon"></i>Create
+          <svg width="30" height="30" viewBox="0 0 36 36">
+            <path fill="#34A853" d="M16 16v14h4V20z"></path>
+            <path fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
+            <path fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
+            <path fill="#EA4335" d="M20 16V6h-4v14z"></path>
+            <path fill="none" d="M0 0h36v36H0z"></path>
+          </svg>
+          Create
         </button>
         <div className="navigation">
           <button
